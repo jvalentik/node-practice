4270ad972f25911d6cd14aeef0fa0e68239f4f5b
jvalentik@sk.ibm.com <SpringBoot3>
2016-06-11 16:56:20 +0200

    webserver - promises

--- server.js
+++ server.js
@@ -13,31 +13,48 @@ let mimeTypes = {
     '.jpeg': 'image/jpeg'
 }
 
-function webServer(req, res) {
-    let baseURI = url.parse(req.url, true);
-    let filePath = __dirname + (baseURI.pathname === '/' ? '/public/index.htm' :'/public/' + baseURI.pathname);
-    fs.access(filePath, fs.F_OK, error => {
-        if(!error) {
-            console.log('Serving: ' + filePath);
-            fs.readFile(filePath, (error, content) => {
-                if(!error) {
-                    let contentType = mimeTypes[path.extname(filePath)];
-                    res.writeHead(200, {'Content-type': contentType});
-                    res.end(content, 'utf-8');
-                }
-                else {
-                    res.writeHead(500);
-                    res.end('The server could not read file');
-                }
-            });
-        }
-        else {
-            res.writeHead(404, {'Content-type': 'text/html'});
-            res.end('Content not found');
-        }
+function fileAccess(filePath) {
+    return new Promise((resolve, reject) => {
+        fs.access(filePath, fs.F_OK, error => {
+            if (!error) {
+                resolve(filePath);
+            }
+            else {
+                reject(error);
+            }
+        });
     });
 }
 
-http.createServer(webServer).listen(3000, () => {
-    console.log('Server running on port 3000');
-});
+function fileReader(filePath) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(filePath, (error, content) => {
+            if(!error) {
+                resolve(content);
+            }
+            else {
+                reject(error);
+            }
+        });
+    });
+}
+
+function webServer(req, res) {
+    let baseURI = url.parse(req.url, true);
+    let filePath = __dirname + (baseURI.pathname === '/' ? '/public/index.htm' : '/public/' + baseURI.pathname);
+    let contentType = mimeTypes[path.extname(filePath)];
+
+    fileAccess(filePath)
+        .then(fileReader)
+        .then(content => {
+            res.writeHead(200, {'Content-type': contentType});
+            res.end(content, 'utf-8');
+        })
+        .catch(error => {
+            res.writeHead(404);
+            res.end(JSON.stringify(error));
+        });
+
+}
+
+http.createServer(webServer).listen(3000, () => console.log('Server running on port 3000'));
